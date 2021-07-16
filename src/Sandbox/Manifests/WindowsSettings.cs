6b69b8c5af1f5f9a2f490c953bffbb912c5fd7fa
Alan Ninan Thomas <75578443+itsme-alan@users.noreply.github.com>
2021-07-17 00:42:03 +0530

    Add File Explorer options in WindowsSettings Manifest (#10)

--- src/Sandbox/Manifests/WindowsSettings.cs
+++ src/Sandbox/Manifests/WindowsSettings.cs
@@ -32,6 +32,19 @@ namespace Sandbox
             context.Resource<RegistryKey>("Disable People in taskbar")
                 .Path(@"HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People\PeopleBand")
                 .Ensure(RegistryKeyState.DoNotExist);
+
+            // File Explorer options
+            context.Resource<RegistryKey>("Show file extensions in File Explorer.")
+                .Path(@"HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\HideFileExt")
+                .Value(0, RegistryKeyValueKind.DWord);
+
+            context.Resource<RegistryKey>("Show hidden files in File Explorer")
+                .Path(@"HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Hidden")
+                .Value(1, RegistryKeyValueKind.DWord);
+
+            context.Resource<RegistryKey>("Show full path in File Explorer title bar")
+                .Path(@"HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState\FullPathAddress")
+                .Value(1, RegistryKeyValueKind.DWord);
         }
     }
 }
