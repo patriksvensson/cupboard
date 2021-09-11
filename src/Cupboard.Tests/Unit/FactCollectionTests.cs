3dab9359edb2b493c77c3b5a7882bdacf755580b
Patrik Svensson <patrik@patriksvensson.se>
2021-09-12 00:00:55 +0200

    Add unit test for ARM and ARM64 check

---
+++ src/Cupboard.Tests/Unit/FactCollectionTests.cs
@@ -0,0 +1,60 @@
+using System.Runtime.InteropServices;
+using Shouldly;
+using Xunit;
+
+namespace Cupboard.Tests.Unit
+{
+    public sealed class FactCollectionTests
+    {
+        public sealed class TheIsArmOrArm64Method
+        {
+            [Fact]
+            public void Should_Return_True_If_ARM()
+            {
+                // Given
+                var facts = new FactCollection
+                {
+                    { "os.arch", Architecture.Arm },
+                };
+
+                // When
+                var result = facts.IsArmOrArm64();
+
+                // Then
+                result.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void Should_Return_True_If_ARM64()
+            {
+                // Given
+                var facts = new FactCollection
+                {
+                    { "os.arch", Architecture.Arm64 },
+                };
+
+                // When
+                var result = facts.IsArmOrArm64();
+
+                // Then
+                result.ShouldBeTrue();
+            }
+
+            [Fact]
+            public void Should_Return_False_If_Not_ARM_OR_ARM64()
+            {
+                // Given
+                var facts = new FactCollection
+                {
+                    { "os.arch", Architecture.X64 },
+                };
+
+                // When
+                var result = facts.IsArmOrArm64();
+
+                // Then
+                result.ShouldBeFalse();
+            }
+        }
+    }
+}
