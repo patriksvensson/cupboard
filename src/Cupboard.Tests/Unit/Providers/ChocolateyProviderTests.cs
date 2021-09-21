eb3158d003e112e6a945f95a9e43454a8ed076a5
Joseph Petersen <josephp90@gmail.com>
2021-09-22 01:36:12 +0200

    add tests

--- src/Cupboard.Tests/Unit/Providers/ChocolateyProviderTests.cs
+++ src/Cupboard.Tests/Unit/Providers/ChocolateyProviderTests.cs
@@ -17,6 +17,17 @@ namespace Cupboard.Tests.Unit.Providers
                 }
             }
 
+            public sealed class InstallVersion : Manifest
+            {
+                public override void Execute(ManifestContext context)
+                {
+                    context.Resource<ChocolateyPackage>("Visual Studio Code")
+                        .Package("vscode")
+                        .Configure(package => package.PackageVersion = "1.58.0")
+                        .Ensure(PackageState.Installed);
+                }
+            }
+
             public sealed class Uninstall : Manifest
             {
                 public override void Execute(ManifestContext context)
@@ -65,6 +76,94 @@ namespace Cupboard.Tests.Unit.Providers
 
                 fixture.Process.Register("choco", "list --limit-output --local-only --exact vscode",
                     new ProcessRunnerResult(0, "vscode|1.58.0\n"));
+
+                // When
+                var result = fixture.Run("-y");
+
+                // Then
+                result.Report.GetState<ChocolateyPackage>("Visual Studio Code").ShouldBe(ResourceState.Unchanged);
+                fixture.Logger.WasLogged("The Chocolatey package [yellow]vscode[/] is already installed");
+            }
+
+            [WindowsFact]
+            public void Should_Install_Package_Version_If_Missing()
+            {
+                // Given
+                var fixture = new CupboardFixture();
+                fixture.Security.IsAdmin = true;
+                fixture.Process.RegisterDefaultResult(new ProcessRunnerResult(0));
+                fixture.Configure(ctx => ctx.UseManifest<Manifests.InstallVersion>());
+
+                fixture.Process.Register("choco", "list --limit-output --local-only --exact vscode",
+                    new ProcessRunnerResult(0, "\n"),
+                    new ProcessRunnerResult(0, "vscode|1.58.0\n"));
+
+                fixture.Process.Register("choco", "install vscode -y --version 1.58.0",
+                    new ProcessRunnerResult(0, "Lol installed VSCode"));
+
+                // When
+                var result = fixture.Run("-y");
+
+                // Then
+                result.Report.GetState<ChocolateyPackage>("Visual Studio Code").ShouldBe(ResourceState.Changed);
+                fixture.Logger.WasLogged("Installing Chocolatey package [yellow]vscode[/]");
+                fixture.Logger.WasLogged("The Chocolatey package [yellow]vscode[/] was installed");
+            }
+
+            [WindowsFact]
+            public void Should_Install_Package_If_Lower_Version()
+            {
+                // Given
+                var fixture = new CupboardFixture();
+                fixture.Security.IsAdmin = true;
+                fixture.Process.RegisterDefaultResult(new ProcessRunnerResult(0));
+                fixture.Configure(ctx => ctx.UseManifest<Manifests.InstallVersion>());
+
+                fixture.Process.Register("choco", "list --limit-output --local-only --exact vscode",
+                    new ProcessRunnerResult(0, "vscode|1.57.0\n"),
+                    new ProcessRunnerResult(0, "vscode|1.58.0\n"));
+
+                fixture.Process.Register("choco", "install vscode -y --version 1.58.0",
+                    new ProcessRunnerResult(0, "Lol installed VSCode"));
+
+                // When
+                var result = fixture.Run("-y");
+
+                // Then
+                result.Report.GetState<ChocolateyPackage>("Visual Studio Code").ShouldBe(ResourceState.Changed);
+                fixture.Logger.WasLogged("Installing Chocolatey package [yellow]vscode[/]");
+                fixture.Logger.WasLogged("The Chocolatey package [yellow]vscode[/] was installed");
+            }
+
+            [WindowsFact]
+            public void Should_Not_Install_Package_Of_Same_Version()
+            {
+                // Given
+                var fixture = new CupboardFixture();
+                fixture.Security.IsAdmin = true;
+                fixture.Configure(ctx => ctx.UseManifest<Manifests.InstallVersion>());
+
+                fixture.Process.Register("choco", "list --limit-output --local-only --exact vscode",
+                    new ProcessRunnerResult(0, "vscode|1.58.0\n"));
+
+                // When
+                var result = fixture.Run("-y");
+
+                // Then
+                result.Report.GetState<ChocolateyPackage>("Visual Studio Code").ShouldBe(ResourceState.Unchanged);
+                fixture.Logger.WasLogged("The Chocolatey package [yellow]vscode[/] is already installed");
+            }
+
+            [WindowsFact]
+            public void Should_Not_Install_Package_Of_Higher_Version()
+            {
+                // Given
+                var fixture = new CupboardFixture();
+                fixture.Security.IsAdmin = true;
+                fixture.Configure(ctx => ctx.UseManifest<Manifests.InstallVersion>());
+
+                fixture.Process.Register("choco", "list --limit-output --local-only --exact vscode",
+                    new ProcessRunnerResult(0, "vscode|1.59.0\n"));
 
                 // When
                 var result = fixture.Run("-y");
