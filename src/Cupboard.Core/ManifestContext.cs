febc8a65523e96b622ff337e59c404a3b3adb30d
Patrik Svensson <patrik@patriksvensson.se>
2022-01-07 12:58:13 +0100

    Add support for adding multiple resources at once

--- src/Cupboard.Core/ManifestContext.cs
+++ src/Cupboard.Core/ManifestContext.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Cupboard
 {
     public sealed class ManifestContext
     {
+        private const BindingFlags Bindings = BindingFlags.Public | BindingFlags.Instance;
+
         public FactCollection Facts { get; }
+
         public List<ResourceBuilder> Builders { get; }
 
         public ManifestContext(FactCollection facts)
@@ -14,14 +19,38 @@ namespace Cupboard
             Builders = new List<ResourceBuilder>();
         }
 
+        public void Resources<TResource>(IEnumerable<TResource> resources, Action<IResourceBuilder<TResource>>? config = null)
+            where TResource : Resource
+        {
+            // Get all public instance properties of the resource
+            // that are both readable and writable.
+            var properties = typeof(TResource)
+                .GetProperties(Bindings)
+                .Where(p => p.CanRead && p.CanWrite);
+
+            foreach (var resource in resources)
+            {
+                var builder = new ResourceBuilder<TResource>(resource.Name);
+                builder.Configure(c =>
+                {
+                    // Shallow clone of the resource
+                    foreach (var property in properties)
+                    {
+                        var value = property.GetValue(resource);
+                        property.SetValue(c, value);
+                    }
+                });
+
+                config?.Invoke(builder);
+                Builders.Add(builder);
+            }
+        }
+
         public IResourceBuilder<TResource> Resource<TResource>(string name)
             where TResource : Resource
         {
             var builder = new ResourceBuilder<TResource>(name);
-
-            // Store a reference to the configuration.
             Builders.Add(builder);
-
             return builder;
         }
     }
---
+++ src/Cupboard.Tests/Unit/ManifestContextTests.cs
@@ -0,0 +1,83 @@
+using System;
+using Cupboard.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace Cupboard.Tests.Unit
+{
+    public sealed class ManifestContextTests
+    {
+        public sealed class Resources
+        {
+            public sealed class DummyResource : Resource
+            {
+                public int Foo { get; set; }
+                public string Bar { get; set; }
+
+                public DummyResource(string name)
+                    : base(name)
+                {
+                }
+            }
+
+            public sealed class DummyResourceProvider : ResourceProvider<DummyResource>
+            {
+                private readonly ICupboardLogger _logger;
+
+                public DummyResourceProvider(ICupboardLogger logger)
+                {
+                    _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+                }
+
+                public override DummyResource Create(string name)
+                {
+                    return new DummyResource(name);
+                }
+
+                public override ResourceState Run(IExecutionContext context, DummyResource resource)
+                {
+                    _logger.Information($"{resource.Name}: Foo={resource.Foo}, Bar={resource.Bar}");
+                    return ResourceState.Executed;
+                }
+            }
+        }
+
+        public sealed class Manifests
+        {
+            public sealed class Default : Manifest
+            {
+                public override void Execute(ManifestContext context)
+                {
+                    context.Resources(new Resources.DummyResource[]
+                    {
+                        new("baz") { Foo = 1, Bar = "BAR" },
+                        new("qux") { Foo = 2, Bar = "QUX" },
+                    });
+                }
+            }
+        }
+
+        [Fact]
+        public void Can_Add_Multiple_Resources_At_Once()
+        {
+            // Given
+            var fixture = new CupboardFixture();
+            fixture.Security.IsAdmin = true;
+            fixture.Process.RegisterDefaultResult(new ProcessRunnerResult(0));
+            fixture.Configure(ctx => ctx.UseManifest<Manifests.Default>());
+            fixture.Register(services =>
+            {
+                services.AddSingleton<IResourceProvider, Resources.DummyResourceProvider>();
+            });
+
+            // When
+            var result = fixture.Run("-y");
+
+            // Then
+            result.ExitCode.ShouldBe(0);
+            fixture.Logger.WasLogged("baz: Foo=1, Bar=BAR");
+            fixture.Logger.WasLogged("qux: Foo=2, Bar=QUX");
+        }
+    }
+}
