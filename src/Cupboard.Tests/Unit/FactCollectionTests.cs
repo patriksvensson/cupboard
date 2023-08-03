4354d4ad3648c49a7e068f17966a86da6d82c91f
Patrik Svensson <patrik@patriksvensson.se>
2023-08-03 09:33:57 +0200

    Upgrade dependencies. Update solution to .NET 7.

--- src/Cupboard.Tests/Unit/FactCollectionTests.cs
+++ src/Cupboard.Tests/Unit/FactCollectionTests.cs
@@ -5,6 +5,58 @@ namespace Cupboard.Tests.Unit
 {
     public sealed class FactCollectionTests
     {
+        [Theory]
+        [InlineData("foo")]
+        [InlineData("FOO")]
+        [InlineData("Foo")]
+        [InlineData("FoO")]
+        public void Should_Consider_Fact_Name_Case_Insensitive(string name)
+        {
+            // Then
+            var c = new FactCollection
+            {
+                { "foo", 1 },
+            };
+
+            // When
+            int value = c[name];
+
+            // Then
+            value.ShouldBe(1);
+        }
+
+        [Fact]
+        public void Should_Get_Value_By_Path_Segments()
+        {
+            // Then
+            var c = new FactCollection
+            {
+                { "foo.bar.baz", 1 },
+            };
+
+            // When
+            int value = c["foo"]["bar"]["baz"];
+
+            // Then
+            value.ShouldBe(1);
+        }
+
+        [Fact]
+        public void Should_Get_Value_By_Full_Path()
+        {
+            // Then
+            var c = new FactCollection
+            {
+                { "foo.bar.baz", 1 },
+            };
+
+            // When
+            int value = c["foo.bar.baz"];
+
+            // Then
+            value.ShouldBe(1);
+        }
+
         public sealed class TheIsArmOrArm64Method
         {
             [Fact]
